3bf2ba5b43b0c0eadcd3b867aeed152ee6b6b5b7
Mohammad Heidari <119068536+Mhdheidari@users.noreply.github.com>
2024-04-04 11:12:43 -0400

    Add files via upload

---
+++ FIM-Hashing-Email.ps1
@@ -0,0 +1,140 @@
+﻿#---------------------- Integrity_Powershell_FIM_Email ----------------------#
+# Hashing Algorithm - File Integrity Monitor(FIM)                            #
+# Collecting new Baseline and Begin monitoring files with saved Baseline     #
+# Notify if a new file has been created, changed, or compromised!            #
+# Sending email with secure password for credential to notify the user       #
+#----------------------------------------------------------------------------#
+
+Function Calculate-File-Hash($filepath) {
+    $filehash = Get-FileHash -Path $filepath -Algorithm SHA512
+    return $filehash
+}
+
+Function Erase-Baseline-If-Already-Exists() {
+    $baselineExists = Test-Path -Path .\baseline.txt
+
+    if ($baselineExists) {
+        # Delete it
+        Remove-Item -Path .\baseline.txt
+    }
+}
+
+Function Send-NotificationEmail($subject, $body) {
+    $sendmbx = "heidarimhd@outlook.com"
+    $receivembx = "heidarimhd@outlook.com"
+
+    $props = @{
+        SMTPServer = "smtp.office365.com"
+        From = $sendmbx
+        To = $receivembx
+        Subject = $subject
+        Body = $body
+        UseSSL = $true
+        Port = 587
+        Credential = New-Object System.Management.Automation.PSCredential($sendmbx,$pwd_2)
+    }
+
+    Send-MailMessage @props
+}
+
+Write-Host ""
+Write-Host "What would you like to do?"
+Write-Host ""
+Write-Host "    A) Collect new Baseline?"
+Write-Host "    B) Begin monitoring files with saved Baseline?"
+Write-Host ""
+$response = Read-Host -Prompt "Please enter 'A' or 'B'"
+Write-Host ""
+
+if ($response -eq "A".ToUpper()) {
+    # Delete baseline.txt if it already exists
+    Erase-Baseline-If-Already-Exists
+
+    # Calculate Hash from the target files and store in baseline.txt
+    # Collect all files in the target folder
+    $files = Get-ChildItem -Path .\FIM
+
+    # For each file, calculate the hash, and write to baseline.txt
+    foreach ($f in $files) {
+        $hash = Calculate-File-Hash $f.FullName
+        "$($hash.Path)|$($hash.Hash)" | Out-File -FilePath .\baseline.txt -Append
+    }
+}
+elseif ($response -eq "B".ToUpper()) {
+    
+    $fileHashDictionary = @{}
+
+    # Load file|hash from baseline.txt and store them in a dictionary
+    $filePathsAndHashes = Get-Content -Path .\baseline.txt
+    
+    foreach ($f in $filePathsAndHashes) {
+         $fileHashDictionary.add($f.Split("|")[0],$f.Split("|")[1])
+    }
+
+    # Begin (continuously) monitoring files with saved Baseline
+    $passwordFile = "C:\Users\Mohammad\PowerShell-Integrity Project\password.txt"
+    $securePassword = Get-Content -Path $passwordFile | ConvertTo-SecureString
+
+    $changedFiles = @{}
+    $createdFiles = @{}
+
+    while ($true) {
+        Start-Sleep -Seconds 1
+        
+        $files = Get-ChildItem -Path .\FIM
+
+        foreach ($f in $files) {
+            $hash = Calculate-File-Hash $f.FullName
+
+            # Notify if a new file has been created
+            if ($fileHashDictionary[$hash.Path] -eq $null -and -not $createdFiles.ContainsKey($hash.Path)) {
+                # A new file has been created!
+                $createdFiles[$hash.Path] = $true
+
+                Write-Host "$($hash.Path) has been created!" -ForegroundColor Green
+
+                $subject = "New File Created: $($hash.Path)"
+                $body = "A new file $($hash.Path) has been created."
+                Send-NotificationEmail $subject $body
+            }
+            elseif ($fileHashDictionary[$hash.Path] -ne $null) {
+                # Check if file hash has changed
+                if ($fileHashDictionary[$hash.Path] -ne $hash.Hash) {
+                    # File has changed
+                    if (-not $changedFiles.ContainsKey($hash.Path)) {
+                        # File has changed and not already notified
+                        $changedFiles[$hash.Path] = $true
+
+                        Write-Host "$($hash.Path) has changed!!!" -ForegroundColor Yellow
+
+                        $subject = "File Changed: $($hash.Path)"
+                        $body = "The file $($hash.Path) has been changed."
+                        Send-NotificationEmail $subject $body
+                    }
+                }
+                else {
+                    # Reset the flag for this file if the hash matches
+                    $changedFiles.Remove($hash.Path)
+                }
+            }
+        }
+
+        # Check for deleted files
+        foreach ($key in $fileHashDictionary.Keys) {
+            $baselineFileStillExists = Test-Path -Path $key
+            if (-Not $baselineFileStillExists) {
+                # File has been deleted
+                if (-not $changedFiles.ContainsKey($key)) {
+                    # File deletion not yet notified
+                    $changedFiles[$key] = $true
+
+                    Write-Host "$($key) has been deleted!" -ForegroundColor DarkRed -BackgroundColor Gray
+
+                    $subject = "File Deleted: $($key)"
+                    $body = "The file $($key) has been deleted."
+                    Send-NotificationEmail $subject $body
+                }
+            }
+        }
+    }
+}
---
+++ SecurePassword-Email.ps1
@@ -0,0 +1,5 @@
+﻿# Convert your password to a secure string
+$securePassword = ConvertTo-SecureString -String "Password" -AsPlainText -Force
+
+# Export the secure password to a file
+$securePassword | ConvertFrom-SecureString | Out-File -FilePath "C:\Users\Mohammad\PowerShell-Integrity Project\password.txt"
